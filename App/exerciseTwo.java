e2b2f6155c073fdde1aa19c275f854c1d6b76316
farzad <farzadafi40@gmail.com>
2022-02-09 20:55:17 +0330

    add exerciseTwo method

--- App/exerciseTwo.java
+++ App/exerciseTwo.java
@@ -4,6 +4,22 @@ import java.util.TreeSet;
 
 public class exerciseTwo {
 
+    public void exerciseTwo() {
+        TreeSet<Character> randomCharacterA = new TreeSet<>();
+        TreeSet<Character> randomCharacterB = new TreeSet<>();
+        TreeSet<Character> union = new TreeSet<>();
+        TreeSet<Character> intersection = new TreeSet<>();
 
+        randomCharacterA.addAll(generateRandomCharacter());
+        randomCharacterB.addAll(generateRandomCharacter());
+        System.out.println( "Tree set 1:" + randomCharacterA);
+        System.out.println( "Tree set 2:" + randomCharacterB);
+
+        union = union(randomCharacterA,randomCharacterB);
+        System.out.println( "Union is:" + union);
+
+        intersection = intersection(randomCharacterA,randomCharacterB);
+        System.out.println( "Intersection is:" + intersection);
+    }
 
 }
