734094db190598f4c6dc49a0a9a14206b1726440
farzad <farzadafi40@gmail.com>
2022-02-09 21:02:59 +0330

    add exerciseThree method

--- App/exerciseThree.java
+++ App/exerciseThree.java
@@ -5,6 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class exerciseThree {
+    int number;
+    Scanner input = new Scanner(System.in);
+    List<Integer> integerList = new ArrayList<>();
 
+    public void exerciseThree() {
+        integerList = readFromInput();
+        System.out.println(integerList);
+
+        List<Integer> arrangeIntegerList = new ArrayList<>();
+        arrangeIntegerList = arrangeList((ArrayList<Integer>) integerList);
+        System.out.println(arrangeIntegerList);
+    }
 
 }
