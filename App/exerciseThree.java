3ab493df2106c153c2ff5da85d2d6573d9c5fdb8
farzad <farzadafi40@gmail.com>
2022-02-09 21:04:54 +0330

    add readFromInput method

--- App/exerciseThree.java
+++ App/exerciseThree.java
@@ -1,12 +1,13 @@
 package App;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class exerciseThree {
     int number;
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
     List<Integer> integerList = new ArrayList<>();
 
     public void exerciseThree() {
@@ -17,5 +18,23 @@ public class exerciseThree {
         arrangeIntegerList = arrangeList((ArrayList<Integer>) integerList);
         System.out.println(arrangeIntegerList);
     }
+
+    public static List<Integer> readFromInput(){
+        int number;
+        List<Integer> integerList = new ArrayList<>();
+        System.out.println("Enter alphabetic for break loop!");
+        for(int i=0; ;i++){
+            System.out.print("Enter " + i + ":");
+            try {
+                number = input.nextInt();
+                integerList.add(number);
+            }catch (InputMismatchException exception){
+                System.out.println("End of the Array!");
+                break;
+            }
+        }
+        return integerList;
+    }
+
 
 }
